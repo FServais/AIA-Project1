bb4d681ccb064ab507576635a594d50c6cc6a90e
Fabrice Servais <fabrice.servais@gmail.com>
2015-10-13 18:48:43 +0200

    Change name variable

--- dt.py
+++ dt.py
@@ -20,12 +20,12 @@ from plot import plot_boundary
 if __name__ == "__main__":
     # (Question 1) dt.py: Decision tree
     SAMPLE_NUMBER = 2000
-    TRAIN_SET_SAMPLE_NUMBER = 150
+    TRAIN_SET_SAMPLE_NUM = 150
 
     X, y = make_data(n_samples=SAMPLE_NUMBER)
 
-    X_train, y_train = X[:TRAIN_SET_SAMPLE_NUMBER], y[:TRAIN_SET_SAMPLE_NUMBER]
-    X_test, y_test = X[TRAIN_SET_SAMPLE_NUMBER:], y[TRAIN_SET_SAMPLE_NUMBER:]
+    X_train, y_train = X[:TRAIN_SET_SAMPLE_NUM], y[:TRAIN_SET_SAMPLE_NUM]
+    X_test, y_test = X[TRAIN_SET_SAMPLE_NUM:], y[TRAIN_SET_SAMPLE_NUM:]
 
     # 1.
     decisionTreeClassifier = DecisionTreeClassifier()
@@ -33,12 +33,12 @@ if __name__ == "__main__":
     y_dtc = decisionTreeClassifier.predict(X_test)
 
     # Plot
-    plot_boundary("Reality", decisionTreeClassifier, X_test, y_test, title="Real data")
-    plot_boundary("Result", decisionTreeClassifier, X_test, y_dtc, title="Result data")
+    plot_boundary("1-1-Ground-Truth", decisionTreeClassifier, X_test, y_test, title="Ground Truth data")
+    plot_boundary("1-1-Prediction", decisionTreeClassifier, X_test, y_dtc, title="Prediction data")
 
 
     # 2.
-    max_depths = [1, 10, 100, 1000, None]
+    max_depths = [1, 10, 50, 100, 1000, None]
     for max_depth in max_depths:
         decisionTreeClassifier = DecisionTreeClassifier(max_depth=max_depth)
         decisionTreeClassifier.fit(X_train, y_train)
@@ -46,6 +46,5 @@ if __name__ == "__main__":
 
         # Plot
         plot_boundary("Reality (%s)" % str(max_depth), decisionTreeClassifier, X_test, y_test, title="Real data (%s)" % str(max_depth))
-
 
     pass
