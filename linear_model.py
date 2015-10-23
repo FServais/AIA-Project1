9cdaec17c85e1f4d1fa2f4280e9c67c1110bd353
Danthine Alexandre <Sacha_2407@MacBook-Pro-dAlexandre.local>
2015-10-23 10:16:33 +0200

    The Q3 is done

--- linear_model.py
+++ linear_model.py
@@ -16,23 +16,35 @@ from sklearn.linear_model import RidgeClassifier
 from data import make_data
 from plot import plot_boundary
 
+
 from utils import get_dataset
 from utils import get_random_state
 from utils import compare
-
-
-
+'''
+def polar_to_cart(X_polar):
+    
+    X_cart_1 =[] 
+    X_cart_2 =[]       
+    for i in range(len(X_polar)):
+        X_cart_1.append(np.sqrt(X_polar[i,0]**2+X_polar[i,1]**2))
+        X_cart_2.append(2* np.arctan(X_polar[i,1]/(X_polar[i,0]+(np.sqrt(X_polar[i,0]**2+X_polar[i,1])))))
+        
+    X_cart = np.concatenate((X_cart_1,X_cart_2),axis=1)
+    
+    return X_cart
+'''
 if __name__ == "__main__":
     # (Question 3) ols.py: Ordinary least square
     
     SAMPLE_NUMBER = 2000
     TRAIN_SET_SAMPLE_NUM = 150
     X, y = get_dataset(SAMPLE_NUMBER)
-
-    X_train, y_train = X[:TRAIN_SET_SAMPLE_NUM], y[:TRAIN_SET_SAMPLE_NUM]
-    X_test, y_test = X[TRAIN_SET_SAMPLE_NUM:], y[TRAIN_SET_SAMPLE_NUM:]
+    X_pol, y_pol = get_dataset(SAMPLE_NUMBER)
     
     #1.
+    
+    X_train, y_train = X[:TRAIN_SET_SAMPLE_NUM], y[:TRAIN_SET_SAMPLE_NUM]
+    X_test, y_test = X[TRAIN_SET_SAMPLE_NUM:], y[TRAIN_SET_SAMPLE_NUM:]
     
     ridgeClassifier = RidgeClassifier(alpha = 0)
     ridgeClassifier.fit(X_train, y_train)
@@ -42,8 +54,33 @@ if __name__ == "__main__":
     plot_boundary("3-1-Ground-Truth", ridgeClassifier, X_test, y_test, title="Ground Truth data")
     plot_boundary("3-1-Prediction", ridgeClassifier, X_test, y_rc, title="Prediction data")
     
-    #TODO: 
     #2.
+    
+    #Change in polar coordinates
+    for i in range(len(X)):
+        X_pol[i,1] = 2* np.arctan(X[i,1]/(X[i,0]+(np.sqrt(X[i,0]**2+X[i,1]**2))))
+        X_pol[i,0] = np.sqrt(X[i,0]**2+X[i,1]**2)
+        
+        if X_pol[i,1] <= X_pol[i,0] - 3./2*np.pi:
+            
+            if X_pol[i,1] <= X_pol[i,0] - 7./2*np.pi:
+                X_pol[i,1] += 4*np.pi
+                continue
+            X_pol[i,1] += 2*np.pi
+            
+    X_train_pol, y_train_pol = X_pol[:TRAIN_SET_SAMPLE_NUM], y_pol[:TRAIN_SET_SAMPLE_NUM]
+    X_test_pol, y_test_pol = X_pol[TRAIN_SET_SAMPLE_NUM:], y_pol[TRAIN_SET_SAMPLE_NUM:]
+    
+    ridgeClassifier = RidgeClassifier(alpha = 0)
+    ridgeClassifier.fit(X_train_pol, y_train_pol)
+    y_rc_pol = ridgeClassifier.predict(X_test_pol)
+    
+    plot_boundary("3-2-Ground-Truth", ridgeClassifier, X_test_pol, y_test_pol, title="Ground Truth data")
+    plot_boundary("3-2-Prediction", ridgeClassifier, X_test_pol, y_rc_pol, title="Prediction data")
+    
+    
+    
+    
     
 
     #3. 
@@ -52,11 +89,12 @@ if __name__ == "__main__":
     
     error_without_transf = compare(y_rc, y_test)
     
-    
     #Accuracy with transformation
     
+    error_with_transf = compare(y_rc_pol, y_test_pol)
     
     #Results
     
-    print("[Q3-3] Error percentage without transformation : {}%".format(error_without_transf/len(y_test)))
+    print("[Q3-3] Error percentage without transformation : {}%".format(error_without_transf*100/len(y_test)))
+    print("[Q3-3] Error percentage with transformation : {}%".format(error_with_transf*100/len(y_test)))
     
